0d68ec66b292a25fa854e04f06b3a0fdeec7b359
edx2799 <jesusdelarosa898@gmail.com>
2024-10-31 21:37:25 -0500

    added calendar
    added calendar to home page, removed home button (since calendar is part of home), and added different color options

--- website/__init__.py
+++ website/__init__.py
@@ -6,7 +6,6 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
@@ -19,7 +18,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User
     
     with app.app_context():
         db.create_all()
@@ -33,9 +32,3 @@ def create_app():
         return User.query.get(int(id))
 
     return app
-
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -1,37 +1,28 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Event
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
-
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
-
-        if len(note) < 1:
-            flash('Note is too short!', category='error') 
-        else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
-            db.session.commit()
-            flash('Note added!', category='success')
-
-    return render_template("home.html", user=current_user)
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
+    if request.method == 'POST':
+        event_data = request.get_json()
+        new_event = Event(
+            title=event_data['title'],
+            start=event_data['start'],
+            color=event_data['color'],
+            user_id=current_user.id
+        )
+        db.session.add(new_event)
+        db.session.commit()
+        return jsonify({'success': True}), 201  # Respond with a success message
+    
+    # For GET requests, return events for the current user
+    user_events = Event.query.filter_by(user_id=current_user.id).all()
+    events_list = [{'id': event.id, 'title': event.title, 'start': event.start.isoformat(), 'color': event.color} for event in user_events]
+    
+    return render_template("home.html", events=events_list, user=current_user)  # Pass current_user to the template
